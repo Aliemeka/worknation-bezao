21d6abc8c3e5ea69acdb7eeb7339d24379728d18
Chukwuemeka Allison <emekaallison4@gmail.com>
2020-11-17 14:15:44 +0100

    Added extra spaces to cards

--- JobApplicationBoard/Models/Applicant.cs
+++ JobApplicationBoard/Models/Applicant.cs
@@ -12,15 +12,18 @@ namespace JobApplicationBoard.Models
         [Key]
         public int ApplicantId { get; set; }
 
-        [Required, MaxLength(25, ErrorMessage = "Cannot exceed 25 characters")]
+        [Required(ErrorMessage = "This field cannot be empty")]
+        [MaxLength(25, ErrorMessage = "Cannot exceed 25 characters")]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
         
-        [Required, MaxLength(25, ErrorMessage = "Cannot exceed 25 characters")]
+        [Required(ErrorMessage = "This field cannot be empty")]
+        [MaxLength(25, ErrorMessage = "Cannot exceed 25 characters")]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
 
-        [EmailAddress(ErrorMessage = "Please enter a valid email"), Required(ErrorMessage = "You have to enter your email")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")] 
+        [Required(ErrorMessage = "You have to enter your email")]
         [Display(Name ="Email address")]
         public string EmailAddress { get; set; }
 
