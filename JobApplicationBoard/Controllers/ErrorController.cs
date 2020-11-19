aa7cec87c7d8ae7c245ab3d4aba805f2a7fe4784
Chukwuemeka Allison <emekaallison4@gmail.com>
2020-11-19 10:14:32 +0100

    Added 404 page

---
+++ JobApplicationBoard/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobApplicationBoard.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error/404")]
+        public IActionResult Error404()
+        {
+            return View("error404");
+        }
+
+        [Route("error/{code:int}")]
+        public IActionResult ErrorPage(int code)
+        {
+            ViewBag.ErrorMessage = "Error " + code; 
+            return View("ErrorPage");
+        }
+    }
+}
--- JobApplicationBoard/Startup.cs
+++ JobApplicationBoard/Startup.cs
@@ -49,15 +49,19 @@ namespace JobApplicationBoard
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCors();
 
             app.UseRouting();
 
             app.UseAuthorization();
             app.UseAuthentication();
 
-            app.UseCors();
+            
 
             app.UseEndpoints(endpoints =>
             {
